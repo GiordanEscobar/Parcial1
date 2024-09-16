710eb47aa53825d9169fc8bea5e47333b3a8c0fb
GiordanEscobar <giordanescobar316992@correo.itm.edu.co>
2024-09-16 18:54:11 -0500

    commit actualizacion GestionReservas GIORDAN

--- AlquilerVehiculos/AlquilerVehiculos/IGestionReservas.cs
+++ AlquilerVehiculos/AlquilerVehiculos/IGestionReservas.cs
@@ -6,8 +6,20 @@ using System.Threading.Tasks;
 
 namespace AlquilerVehiculos
 {
-    internal class IGestionReservas
+    //creamos la interfaz para gestionar las reservas
+    public interface IGestionReservas
     {
+        //se usa para reservar un vehiculo
+        void Reservar(Cliente cliente, Vehiculo vehiculo, DateTime fechainicio, int cantdias);
+
+        //se usa para devolver un vehiculo que estaba alquilado y le cambia el estado a disponible
+        void Devolver(Vehiculo vehiculo);
+
+        //se usa para consultar la lista que contiene el historial de reservas de un vehiculo
+        List <String>ConsultarHistorial(Vehiculo vehiculo);
+
+        //para cancelar las reservas que se hayan hecho
+        bool CancelarReserva(Cliente cLiente, Vehiculo vehiculo);
 
     }
 }
