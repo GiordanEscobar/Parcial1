1579dbe0eb62e16663595d1ed2bccb4794e9f4ce
GiordanEscobar <giordanescobar316992@correo.itm.edu.co>
2024-09-16 17:30:39 -0500

    commit codigo interfaces

---
+++ AlquilerVehiculos/AlquilerVehiculos/IGestionVehiculos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlquilerVehiculos
+{
+    //creamos la interfaz que nos ayudara a gestionar las acciones que podemos hacer con un vehiculo
+    public interface IGestionVehiculos
+    {
+        //se debe añaadir como parametro vehiculo para saber cuál en específico se va a agregar
+        void RegistrarVehiculo(Vehiculo vehiculo); 
+
+        //se usa para eliminar un vehiculo de la lista de vehiculos, se hace booleano para verificar
+
+    }
+}
--- AlquilerVehiculos/AlquilerVehiculos/Vehiculo.cs
+++ AlquilerVehiculos/AlquilerVehiculos/Vehiculo.cs
@@ -9,17 +9,26 @@ namespace AlquilerVehiculos
     //creacion de la clase abstracta
     public abstract class Vehiculo
     {
-        public string? Marca { get; set; }
-        public string? Modelo { get; set; }
+        public String? Marca { get; set; }
+        public String? Modelo { get; set; }
         public int AñoDeFabricacion { get; set; }
         //variable del precio del alquiler del vehículo
         public double ValorAlquiler { get; set; }
         //variable de tipo Estado para gestionar la disponibilidad del vehiculo
         public Estado estado { get; set; }
+         
+        public Vehiculo(String marca, String modelo, int añofabricacion, double valoralquiler)
+        {
+            Marca= marca;
+            Modelo= modelo;
+            AñoDeFabricacion = añofabricacion;
+            ValorAlquiler= valoralquiler;
+            //se inicia en disponible por defecto
+            this.estado = Estado.Disponible;
+        }
 
-
-        //creacion del metodo abstracto que nos permite mostrar la informacion del véhículo
-        public abstract void VerInformacion();
+        //creacion del metodo abstracto que nos permite ver la informacion del véhículo
+        public abstract void VerInfo();
 
     }
 }
