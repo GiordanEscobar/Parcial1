7e44e5ab03a123df58a76efcbd223c48d6e25e7f
GiordanEscobar <giordanescobar316992@correo.itm.edu.co>
2024-09-16 13:30:33 -0500

    commit3 Giordan

--- AlquilerVehiculos/AlquilerVehiculos/Vehiculo.cs
+++ AlquilerVehiculos/AlquilerVehiculos/Vehiculo.cs
@@ -9,13 +9,17 @@ namespace AlquilerVehiculos
     //creacion de la clase abstracta
     public abstract class Vehiculo
     {
-        public string? Marca {  get; set; }
+        public string? Marca { get; set; }
         public string? Modelo { get; set; }
-        public int AñoDeFabricacion {  get; set; }
+        public int AñoDeFabricacion { get; set; }
         //variable del precio del alquiler del vehículo
-        public double ValorAlquiler {  get; set; }
+        public double ValorAlquiler { get; set; }
         //variable de tipo Estado para gestionar la disponibilidad del vehiculo
         public Estado estado { get; set; }
+
+
+        //creacion del metodo abstracto que nos permite mostrar la informacion del véhículo
+        public abstract void VerInformacion();
 
     }
 }
